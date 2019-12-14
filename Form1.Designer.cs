92466e3cefa87ecf0c97d2dfd75a8761c984b775
Ziad-Mahmoud <37977244+Ziad-Mahmoud@users.noreply.github.com>
2019-12-14 23:33:06 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace tryingNew_Ideas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Simple = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Average = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Complex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Simple,
+            this.Average,
+            this.Complex});
+            this.dataGridView1.Location = new System.Drawing.Point(260, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(458, 90);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Simple
+            // 
+            this.Simple.HeaderText = "Simple";
+            this.Simple.Name = "Simple";
+            // 
+            // Average
+            // 
+            this.Average.HeaderText = "Average";
+            this.Average.Name = "Average";
+            // 
+            // Complex
+            // 
+            this.Complex.HeaderText = "Complex";
+            this.Complex.Name = "Complex";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(63, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "complexity_table";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(742, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 53);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(74, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(972, 139);
+            this.panel1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 44);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "UFP";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(74, 157);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(870, 139);
+            this.panel2.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkCyan;
+            this.button3.Location = new System.Drawing.Point(293, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(267, 61);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(227, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Rate";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(329, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(157, 24);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 44);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "DI";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(592, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 53);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Heavly Utilized Hardware";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Location = new System.Drawing.Point(74, 302);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(870, 139);
+            this.panel3.TabIndex = 15;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.DarkCyan;
+            this.button4.Location = new System.Drawing.Point(293, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(267, 73);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Calculate FP";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label7.Location = new System.Drawing.Point(12, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 44);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "FP";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(227, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "label8";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label10);
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Controls.Add(this.comboBox2);
+            this.panel4.Controls.Add(this.button6);
+            this.panel4.Location = new System.Drawing.Point(74, 447);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(870, 136);
+            this.panel4.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold);
+            this.label10.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label10.Location = new System.Drawing.Point(12, 13);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 44);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "LOC";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(173, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Value";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(278, 32);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(265, 24);
+            this.comboBox2.TabIndex = 15;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox2_KeyDown);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(592, 18);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(84, 53);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Select";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(849, 38);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 77);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Done";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1049, 595);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Function Point Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Simple;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Average;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Complex;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tryingNew_Ideas
+{
+    public partial class Form1 : Form
+    {
+        //############################# Global Variables ###############################
+
+        // Create a dictionary of integers, with string keys for AVC values for each language in DDL
+        Dictionary<string, int> AVC_table = new Dictionary<string, int>();
+
+        // Create another dictionary of integers, with string keys for GSC
+        Dictionary<string, int> GSC_table = new Dictionary<string, int>();
+
+        private Dictionary<string, int>.KeyCollection icoll, icoll_2;   // is a dictionary to 
+
+
+        string[] TCF_Array = { "Data Communication", "Distributed Data Process",
+                               "Preformance Criteria", "Heavly Utilized Hardware",
+                               "High Transaction Rates", "Online Data Entery",
+                               "Online Updating", "End-user Efficiency",
+                               "Complex Computation", "Reusability",
+                               "Ease of Installation", "Ease of Operation",
+                               "Portability", "Maintaenability"};
+
+        string[,] complexity_table = new string[,]
+        {               {"External Interface Files" , "5", "7" , "10" },
+                        {"Internal Logical Files"   , "7", "10", "15" },
+                        {"External Input"           , "3", "4" , "6"  },
+                        {"External Output"          , "4", "5" , "7"  },
+                        {"External Inquiry"         , "3", "4" , "6"  }
+        };
+
+        int counter = 0;
+        int btn2_Counter = 0;
+        int Total_UFP = 0;
+        int total_DI = 0;
+        double total_TCF = 0;
+        double FP_total = 0;
+
+        //###############################################################################
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Adding key/value pairs in AVC_table Dictionariy 
+            AVC_table.Add("Assembly Language", 320);
+            AVC_table.Add("C", 128);
+            AVC_table.Add("COBOL/Fortran", 105);
+            AVC_table.Add("Pascal", 90);
+            AVC_table.Add("Ada", 70);
+            AVC_table.Add("C++", 64);
+            AVC_table.Add("Visual Basic", 32);
+            AVC_table.Add("Object-Oriented Language", 30);
+            AVC_table.Add("Smalltalk", 22);
+            AVC_table.Add("Code Generators (Power Builder)", 15);
+            AVC_table.Add("SQL/Oracle", 12);
+            AVC_table.Add("Spreadsheets", 6);
+            AVC_table.Add("Graphical Language (icons)", 4);
+            //**************************************************//
+
+            // Adding key/value pairs in GSC_table Dictionariy
+            GSC_table.Add("No Influence", 0);
+            GSC_table.Add("Incidental", 1);
+            GSC_table.Add("Moderate", 2);
+            GSC_table.Add("Average", 3);
+            GSC_table.Add("Significant", 4);
+            GSC_table.Add("Essential", 5);
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            panel2.Visible = false;
+            panel3.Visible = false;
+            panel4.Visible = false;
+
+            label1.Text = complexity_table[counter, 0];
+
+            string[] row = new string[complexity_table.GetLength(1)];
+
+            for (int j = 1; j < complexity_table.GetLength(1); j++)
+            {
+                row[j - 1] = complexity_table[counter, j];
+            }
+
+            dataGridView1.Rows.Add(row);
+            // MessageBox.Show(rows.GetLength(0).ToString());
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && dataGridView1.CurrentCell.Value.ToString() != null)
+            {
+                Total_UFP += Int32.Parse(textBox1.Text) * Int32.Parse(dataGridView1.CurrentCell.Value.ToString());
+
+                dataGridView1.Rows.Clear();
+                textBox1.Text = "";
+                if (counter < complexity_table.GetLength(0) - 1) // add 1 every click on button_1 untill it equals # rows of 2D_array
+                {
+                    counter++;
+                    label1.Text = complexity_table[counter, 0];
+
+                    string[] row = new string[complexity_table.GetLength(1)];
+                    for (int j = 1; j < complexity_table.GetLength(1); j++)
+                    {
+                        row[j - 1] = complexity_table[counter, j];
+                    }
+                    dataGridView1.Rows.Add(row);
+                }
+                else
+                {
+                    dataGridView1.Visible = false;
+                    button1.Visible = false;
+                    textBox1.Visible = false;
+
+                    //MessageBox.Show("Total UFP = " + Total_UFP.ToString()); 
+                    label3.Visible = false;
+
+                    label1.Text = ("Total UFP = " + Total_UFP.ToString());
+                    label1.ForeColor = Color.DarkCyan;
+                    label1.Font = new Font("", 22, FontStyle.Bold);
+                    label1.Location = new Point(224, 10);
+
+                    panel2.Visible = true;
+                    button3.Visible = false;
+
+                    label2.Text = TCF_Array[0];
+
+                    icoll = GSC_table.Keys;
+                    foreach (string s in icoll)
+                    {
+                        comboBox1.Items.Add(s);
+                    }
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fill all fields!!!!");
+
+            }
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this, new EventArgs());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            total_DI += GSC_table[comboBox1.Text];
+
+            if (btn2_Counter < TCF_Array.Length - 1)
+            {
+                label2.Text = TCF_Array[btn2_Counter + 1]; // array start from 1
+                btn2_Counter++;
+            }
+            else
+            {
+                MessageBox.Show("ended");
+
+                button2.Visible = false;
+                label2.Visible = false;
+                comboBox1.Visible = false;
+                label5.Visible = false;
+
+                label4.Text = "DI = " + total_DI.ToString();
+
+                button3.Visible = true;
+                button3.Text = "Calculate TCF";
+
+
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show(comboBox1.SelectedText);
+            label5.Text = GSC_table[comboBox1.Text].ToString();
+
+        }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button2_Click(this, new EventArgs());
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            total_TCF = 0.65 + (0.01 * total_DI);
+
+            label5.ForeColor = Color.DarkCyan;
+            label5.Font = new Font("", 22, FontStyle.Bold);
+            label5.Text = ("Total TCF = " + total_TCF.ToString());
+            label5.Visible = true;
+            label5.Location = new Point(224, 10);
+
+            button3.Visible = false;
+            label4.Visible = false;
+            panel3.Visible = true;
+            label8.Visible = false;
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label6.Visible = true;
+            label6.Text = AVC_table[comboBox2.Text].ToString();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label10.Visible = false;
+            comboBox2.Visible = false;
+            button6.Visible = false;
+
+            double LOC_total = FP_total * AVC_table[comboBox2.Text]; 
+            label6.ForeColor = Color.DarkCyan;
+            label6.Font = new Font("", 22, FontStyle.Bold);
+            label6.Text = ("LOC = " + LOC_total.ToString());
+            label6.Visible = true;
+            label6.Location = new Point(224, 10);
+        }
+
+        private void comboBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button6_Click(this, new EventArgs());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FP_total = (Total_UFP * total_TCF);
+
+            label8.ForeColor = Color.DarkCyan;
+            label8.Font = new Font("", 22, FontStyle.Bold);
+            label8.Text = ("FP = UFP * TCF = " + (FP_total).ToString());
+            label8.Visible = true;
+            label8.Location = new Point(150, 10);
+
+            button4.Visible = false;
+            label7.Visible = false;
+
+            panel4.Visible = true;
+            label6.Visible = false;
+
+            icoll_2 = AVC_table.Keys;
+            foreach (string s in icoll_2)
+            {
+                comboBox2.Items.Add(s);
+            }
+
+        }
+    }
+
+}
