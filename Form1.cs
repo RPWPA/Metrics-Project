96e88d1cb6dd430aaaf6fe6f8568766225e8b7b7
Ziad-Mahmoud <37977244+Ziad-Mahmoud@users.noreply.github.com>
2019-12-16 19:35:05 +0200

    Modified - Simplified the Code and GUI. (1st)

--- Form1.cs
+++ Form1.cs
@@ -39,7 +39,6 @@ namespace tryingNew_Ideas
                         {"External Inquiry"         , "3", "4" , "6"  }
         };
 
-        int counter = 0;
         int btn2_Counter = 0;
         int Total_UFP = 0;
         int total_DI = 0;
@@ -80,84 +79,60 @@ namespace tryingNew_Ideas
         }
 
 
+
+
+
+        //********************************************************************************//
+        // ****************************     Calculate UFP    *****************************//
+        //********************************************************************************//
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            button1.Visible = false;
             panel2.Visible = false;
             panel3.Visible = false;
             panel4.Visible = false;
 
-            label1.Text = complexity_table[counter, 0];
+            // filling comboBox_3 with complexity_table values
+            for (int i = 0; i < complexity_table.GetLength(0); i++)
+            {
+                comboBox3.Items.Add(complexity_table[i, 0]);
+            }
+        }
 
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
             string[] row = new string[complexity_table.GetLength(1)];
 
             for (int j = 1; j < complexity_table.GetLength(1); j++)
             {
-                row[j - 1] = complexity_table[counter, j];
+                row[j - 1] = complexity_table[this.comboBox3.SelectedIndex, j];
             }
 
-            dataGridView1.Rows.Add(row);
-            // MessageBox.Show(rows.GetLength(0).ToString());
+            dataGridView1.Rows.Insert(0, row);
+            if (dataGridView1.Rows.Count == 2)   // to remove the old rows
+            {
+                dataGridView1.Rows.RemoveAt(1);
+            }
         }
 
-
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)  // each time, multiplies textBox1 & comboBox3 values then add result in Total_UFP
         {
-            if (textBox1.Text != "" && dataGridView1.CurrentCell.Value.ToString() != null)
+            if (textBox1.Text != "" && comboBox3.Text != "" && dataGridView1.CurrentCell.Value.ToString() != null)
             {
                 Total_UFP += Int32.Parse(textBox1.Text) * Int32.Parse(dataGridView1.CurrentCell.Value.ToString());
 
                 dataGridView1.Rows.Clear();
                 textBox1.Text = "";
-                if (counter < complexity_table.GetLength(0) - 1) // add 1 every click on button_1 untill it equals # rows of 2D_array
-                {
-                    counter++;
-                    label1.Text = complexity_table[counter, 0];
-
-                    string[] row = new string[complexity_table.GetLength(1)];
-                    for (int j = 1; j < complexity_table.GetLength(1); j++)
-                    {
-                        row[j - 1] = complexity_table[counter, j];
-                    }
-                    dataGridView1.Rows.Add(row);
-                }
-                else
-                {
-                    dataGridView1.Visible = false;
-                    button1.Visible = false;
-                    textBox1.Visible = false;
-
-                    //MessageBox.Show("Total UFP = " + Total_UFP.ToString()); 
-                    label3.Visible = false;
-
-                    label1.Text = ("Total UFP = " + Total_UFP.ToString());
-                    label1.ForeColor = Color.DarkCyan;
-                    label1.Font = new Font("", 22, FontStyle.Bold);
-                    label1.Location = new Point(224, 10);
-
-                    panel2.Visible = true;
-                    button3.Visible = false;
-
-                    label2.Text = TCF_Array[0];
-
-                    icoll = GSC_table.Keys;
-                    foreach (string s in icoll)
-                    {
-                        comboBox1.Items.Add(s);
-                    }
-
-
-                }
+                comboBox3.Text = "";
             }
             else
             {
                 MessageBox.Show("Fill all fields!!!!");
-
             }
-
         }
 
-        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)   
         {
             if (e.KeyCode == Keys.Enter)
             {
@@ -165,46 +140,84 @@ namespace tryingNew_Ideas
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
         {
-
-            total_DI += GSC_table[comboBox1.Text];
-
-            if (btn2_Counter < TCF_Array.Length - 1)
+            if (e.KeyCode == Keys.Enter)
             {
-                label2.Text = TCF_Array[btn2_Counter + 1]; // array start from 1
-                btn2_Counter++;
-            }
-            else
-            {
-                MessageBox.Show("ended");
-
-                button2.Visible = false;
-                label2.Visible = false;
-                comboBox1.Visible = false;
-                label5.Visible = false;
-
-                label4.Text = "DI = " + total_DI.ToString();
-
-                button3.Visible = true;
-                button3.Text = "Calculate TCF";
-
-
+                button1_Click(this, new EventArgs());
             }
         }
 
+        private void label3_Click(object sender, EventArgs e)   //Finally show UFP resulte by click on label_3
+        {
+            DialogResult dialogResult = MessageBox.Show("Calculate UFP Now !!", "", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //do something
+                dataGridView1.Visible = false;
+                button1.Visible = false;
+                textBox1.Visible = false;
+                comboBox3.Visible = false;
+                button3.Visible = false;
+                //MessageBox.Show("Total UFP = " + Total_UFP.ToString()); 
+
+                label3.Text = ("Total UFP = " + Total_UFP.ToString());
+                label3.ForeColor = Color.DarkCyan;
+                label3.Font = new Font("", 22, FontStyle.Bold);
+                label3.Location = new Point(224, 10);
+
+                panel2.Visible = true;
+
+                label2.Text = TCF_Array[0];
+
+                icoll = GSC_table.Keys;
+                foreach (string s in icoll)
+                {
+                    comboBox1.Items.Add(s);
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                MessageBox.Show("continue adding ufp");
+            }
+        }
+
+
+
+        //********************************************************************************//
+        // ************************      Calculate DI & TCF    ***************************//
+        //********************************************************************************//
+
+
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //MessageBox.Show(comboBox1.SelectedText);
             label5.Text = GSC_table[comboBox1.Text].ToString();
-
         }
 
         private void comboBox1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
             {
-                button2_Click(this, new EventArgs());
+                total_DI += GSC_table[comboBox1.Text];
+
+                if (btn2_Counter < (TCF_Array.Length - 1) )
+                {
+                    label2.Text = TCF_Array[btn2_Counter + 1]; // array start from 1
+                    btn2_Counter++;
+                }
+                else
+                {
+                    MessageBox.Show("ended");
+
+                    label4.Text = "DI = " + total_DI.ToString();
+
+                    button3.Visible = true;
+                    button3.Text = "Calculate TCF";
+
+                    comboBox1.Visible = false;
+                    label5.Visible = false;
+                    label2.Visible = false;
+                }
             }
         }
 
@@ -222,51 +235,23 @@ namespace tryingNew_Ideas
             button3.Visible = false;
             label4.Visible = false;
             panel3.Visible = true;
-            label8.Visible = false;
-
         }
 
-        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            label6.Visible = true;
-            label6.Text = AVC_table[comboBox2.Text].ToString();
 
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            label10.Visible = false;
-            comboBox2.Visible = false;
-            button6.Visible = false;
-
-            double LOC_total = FP_total * AVC_table[comboBox2.Text]; 
-            label6.ForeColor = Color.DarkCyan;
-            label6.Font = new Font("", 22, FontStyle.Bold);
-            label6.Text = ("LOC = " + LOC_total.ToString());
-            label6.Visible = true;
-            label6.Location = new Point(224, 10);
-        }
-
-        private void comboBox2_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                button6_Click(this, new EventArgs());
-            }
-        }
-
+        //********************************************************************************//
+        // ****************************      Calculate FP    *****************************//
+        //********************************************************************************//
         private void button4_Click(object sender, EventArgs e)
         {
             FP_total = (Total_UFP * total_TCF);
 
-            label8.ForeColor = Color.DarkCyan;
-            label8.Font = new Font("", 22, FontStyle.Bold);
-            label8.Text = ("FP = UFP * TCF = " + (FP_total).ToString());
-            label8.Visible = true;
-            label8.Location = new Point(150, 10);
+            label7.ForeColor = Color.DarkCyan;
+            label7.Font = new Font("", 22, FontStyle.Bold);
+            label7.Text = ("FP = UFP * TCF = " + (FP_total).ToString());
+            label7.Visible = true;
+            label7.Location = new Point(180, 10);
 
             button4.Visible = false;
-            label7.Visible = false;
 
             panel4.Visible = true;
             label6.Visible = false;
@@ -276,8 +261,33 @@ namespace tryingNew_Ideas
             {
                 comboBox2.Items.Add(s);
             }
-
         }
+
+
+        //********************************************************************************//
+        // ***************************      Calculate LOC    *****************************//
+        //********************************************************************************//
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label6.Visible = true;
+            label6.Text = AVC_table[comboBox2.Text].ToString();
+        }
+
+        private void comboBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                double LOC_total = FP_total * AVC_table[comboBox2.Text];
+                label6.ForeColor = Color.DarkCyan;
+                label6.Font = new Font("", 22, FontStyle.Bold);
+                label6.Text = ("LOC = " + LOC_total.ToString());
+                label6.Visible = true;
+                label6.Location = new Point(235, 10);
+
+                label10.Visible = false;
+                comboBox2.Visible = false;
+            }
+        }  
     }
 
 }
